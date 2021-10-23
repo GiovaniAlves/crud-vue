142bb9b785c27556ebe533cd4582544000b6e121
giovani.alves <giovani.alves.glv@gmail.com>
2021-10-23 16:22:29 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">TASK MANAGER (VUE)</h1>
+                <p class="lead">Using the Axios library to make Ajax calls to a REST API.</p>
+            </div>
+        </div>
+
+        <div class="container">
+
+        </div>
+
+    </div>
+</template>
+
